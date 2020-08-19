3864e4a2aa2b75ac279df5bb880f20dca09e6411
vagrant <vagrant@localhost.localdomain>
2020-08-19 07:27:14 +0000

    add:routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/show'
-  get 'orders/new'
-  get 'deliveries/edit'
-  get 'deliveries/index'
-  get 'cart_items/index'
-  get 'items/index'
-  get 'items/edit'
-  get 'items/show'
-  get 'customers/index'
-  get 'customers/show'
-  get 'customers/new'
-  get 'customers/edit'
-  devise_for :admins
   devise_for :customers
+  devise_for :admins
+  resources :orders :only: [:index, :show, :new]
+  resources :deliveries :only [:edit, :index]
+  resources :cart_items :only [:index]
+  resources :items :only [:index,:edit, :show]
+  resources :customers :only [:index, :show, :new, :edit]
+
+  root to: 'homes#top'
+  get 'home/about', to: 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
