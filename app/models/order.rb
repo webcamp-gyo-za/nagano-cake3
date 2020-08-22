a4ef9a14d3369699c6586ac49adfdaa4846ac1f6
vagrant <vagrant@localhost.localdomain>
2020-08-22 07:50:02 +0000

    test

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,11 @@
 class Order < ApplicationRecord
+	validates :post_number, presence: true
+	validates :adress, presence: true
+	validates :name, presence: true
+	belongs_to :user
+	has_many :order_details, dependent: :destroy
+
+	def subtotal
+		(cart_item.price*cart_item.amount*1.1).round
+	end
 end
