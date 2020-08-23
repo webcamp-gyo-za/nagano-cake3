4d261a0a5081146718c112029d8d5a6311def8b3
vagrant <vagrant@localhost.localdomain>
2020-08-23 05:47:09 +0000

    tachi

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+
+  #before_action :authenticate_customer!
+
   def new
   	@order=Order.new
   	@order.customer_id=current_customer.id
@@ -8,28 +11,49 @@ class OrdersController < ApplicationController
   def confirm
   	@order=Order.new(order_params)
   	@order.order_detail.new
-  	@cart_item=current_user.cart_item
+  	@cart_item=current_customer.cart_item #要確認
+
+    @cart_item=current_customer.cart_item
+
+  @order_price　= 0
+  @cart_items.each do |f|
+    @order_price +=f.subtotal
+  end
+
+  @shipping_cost.order_price = @order_price + 800
+ end
 
   def create
   	@order=Order.new(order_params)
   	if @list.save
   	   redirect_to orders_success_path
   	else
-  	   @order.user_id=current_user.id
+  	   @order.customer_id=current_user.id
   	   #@user=currrent_user
   	   render "new"
   	end
   end
 
+  
+
   def index
-  	@order=Order.where(customer_id :cueernt_customer.id).order(created_at: :desc)
+  	@order=Order.where(customer_id :current_customer.id).order(created_at: :desc)
   	@item=@order.item
   end
 
   def show
   	@order=Order.find(params[:id])
   	@cart_item=current_customer.cart_item
+
+  @order_price　= 0
+  @cart_items.each do |f|
+     @order_price +=f.subtotal
   end
+
+  @shipping_cost.order_price = @order_price + 800
+ end
+
+  
 
   def thanks
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,13 @@
 class Order < ApplicationRecord
+	belongs_to :customer
+	has_many :order_details, dependent: :destroy
+
 	validates :post_number, presence: true
 	validates :adress, presence: true
 	validates :name, presence: true
-	belongs_to :user
-	has_many :order_details, dependent: :destroy
 
+	enum payment_method: {"クレジットカード": 0,"銀行振込": 1}
+	enum status: {"入金待ち": 0, "入金確認": 1, "製作中": 2, "発送準備中": 3, "発送済み": 4}
 	def subtotal
 		(cart_item.price*cart_item.amount*1.1).round
 	end
