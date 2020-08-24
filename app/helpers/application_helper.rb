4bc792850abefd329e7db42d6554e400a704d90c
vagrant <vagrant@localhost.localdomain>
2020-08-24 04:32:34 +0000

    cart_item

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+	def price_include_tax(price)
+	  price = price * 1.08
+	  "#{price.floor}円"
+	end
+
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,6 @@
 class CartItem < ApplicationRecord
+	belongs_to :customer
+	belongs_to :item
+	validates :customer_id, presence: true
+	validates :item_id, presence: true
 end
