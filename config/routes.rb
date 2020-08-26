b6823e1976632c8cdb4f0ff7c750e388e5ae0dbe
futa1952 <futa1952@icloud.com>
2020-08-26 23:42:06 +0900

    update:退会機能、デザイン修正

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ Rails.application.routes.draw do
     get 'item/about', to: 'items#about'
     get 'search/search'
     get 'withdrawal' => 'customers#withdrawal'
+    put "/customers/:id/hide" => "customers#hide", as: 'customers_hide'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
